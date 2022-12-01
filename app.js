50759a09d3da3dfff1096a042c0202d6ccd5f462
= <=>
2022-12-01 15:28:35 -0300

    Separando rotas e serviços

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-import mysql from 'mysql2';
-import express from 'express';
-import { engine } from 'express-handlebars';
+
+const express = require ('express');
+const { engine } = require ('express-handlebars');
+const rotas = require('./rotas/index');
+const db = require('./services/index');
 var porta = 3000;
 
 var app = express();
@@ -17,97 +19,14 @@ app.use(
 
 app.use(express.json());
 
-app.use(express.static('public'));
+app.use("/public", express.static(__dirname + '/public'));
 
-app.post('/insert', (req, res) => {
-    const title = req.body.title;
-    const pages = req.body.pages;
+app.use(rotas);
 
-    const query = `INSERT INTO books (title, pages) VALUES ('${title}', '${pages}')`;
-
-    conn.query(query, function(err) {
-        if (err) {
-            console.log(err);
-        }
-
-        res.redirect('/');
-    });
-});
-
-app.get('/', (req, res) => {
-    const query = 'SELECT * FROM books';
-
-    conn.query(query, function(err, data) {
-        if (err) {
-            console.log(err);
-            return;
-        }
-
-        const books = data;
-		
-		res.render('home', { books });
-    });
-});
-
-app.get('/delete/:idbooks', function (req, res) {
-    const { idbooks } = req.params;
-
-    const query = 'DELETE FROM books WHERE idbooks = ?';
-
-    conn.query(query, idbooks, function(err) {
-        if (err) {
-            console.log(err);
-        }
-
-        res.redirect('/');
-    });
-});
-
-app.get('/edit/:idbooks?', (req, res) => {
-    const { idbooks } = req.params;
-    const query = `SELECT * FROM books WHERE idbooks = '${idbooks}'`;
-
-    conn.query(query, function(err, data) {
-        if (err) {
-            console.log(err);
-            return;
-        }
-
-        const book = data;
-		
-		res.render('edit', { book: book });
-    });
-});
-
-app.post('/edit', (req, res) => {
-    const idbooks = req.body.idbooks;
-    const title = req.body.title;
-    const pages = req.body.pages;
-
-    const query = `UPDATE books SET title = '${title}', pages = '${pages}' WHERE idbooks = '${idbooks}'`;
-
-    conn.query(query, function(err) {
-        if (err) {
-            console.log(err);
-        }
-
-        res.redirect('/');
-    });
-});
-
-const conn = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '', //Coloca a tua senha aqui
-    database: 'nodemysql1'
-});
-
-conn.connect(function(err) {
-    if (err) {
-        console.log(err);
-    }
-
+if (db.conn) {
     console.log('Conectado ao MySQL');
-
     app.listen(porta);
-});
+} else {
+    console.log('Erro ao conectar ao MySQL');
+}
+
